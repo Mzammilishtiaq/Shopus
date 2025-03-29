83f37a078648a0e9e6517db43c19b1335c982ef4
Mzammilishtiaq <tomuzammilishtiaq@gmail.com>
2025-03-28 23:58:29 -0700

    next.config ad

--- next.config.js
+++ next.config.js
@@ -2,9 +2,10 @@
 const runtimeCaching = require("next-pwa/cache");
 const withPWA = require('next-pwa');
 const { hostname } = new URL(`${process.env.NEXT_PUBLIC_BASE_URL}`);
-const nextConfig={
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'standalone',
   images: {
     domains: [`${hostname}`]
   },
@@ -16,7 +17,7 @@ const nextConfigWithPwa = withPWA({
   skipWaiting: true,
   runtimeCaching,
   disable: process.env.NODE_ENV === "development",
-}) (
+})(
     nextConfig
 );
-module.exports = pwa==='1'?nextConfigWithPwa:nextConfig ;
+module.exports = pwa === '1' ? nextConfigWithPwa : nextConfig;
